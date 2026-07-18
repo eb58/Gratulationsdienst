69c736a4bf290d13b1ff6fc0f64dc7d8af18157d
eb58 <erich.brandl@gmail.com>
2026-07-18 14:52:44 +0200

    Hyphenate data protection regulation

--- modules/sokoPrivacyText.js
+++ modules/sokoPrivacyText.js
@@ -3,7 +3,7 @@ import { escapeHtml } from './utils.js';
 export const SOKO_PRIVACY_TEXT = String.raw`
   Das Soko-Mitglied hat die betroffene Person darauf hingewiesen, dass
   die im Rahmen der vorstehend genannten Zwecke erhobenen personenbezogenen Daten unter
-  Beachtung der EU-Datenschutzgrundverordnung und des Berliner Datenschutzgesetzes erhoben,
+  Beachtung der EU-Datenschutz-Grundverordnung und des Berliner Datenschutzgesetzes erhoben,
   verarbeitet und genutzt werden. Zudem wurde darauf hingewiesen, dass die Erhebung,
   Verarbeitung und Nutzung ihrer Daten auf freiwilliger Basis erfolgt und die Einwilligung auch
   verweigert werden kann. Die Verweigerung der Einwilligung führt dazu, dass keine Pressemitteilung
