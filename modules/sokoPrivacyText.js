ab784c459a28489cb4779c6c232e089ccc165fc4
eb58 <erich.brandl@gmail.com>
2026-07-18 14:49:10 +0200

    Refine SOKO privacy text definition

--- modules/sokoPrivacyText.js
+++ modules/sokoPrivacyText.js
@@ -1,16 +1,15 @@
 import { escapeHtml } from './utils.js';
 
-const lines = [
-  'Die Soko-Mitarbeiterin/der Soko-Mitarbeiter hat die Jubilarin/den Jubilar darauf hingewiesen, dass',
-  'die im Rahmen der vorstehend genannten Zwecke erhobenen persönlichen Daten Ihrer Person unter',
-  'Beachtung der EU-Datenschutzgrundverordnung und des Berliner Datenschutzgesetzes erhoben,',
-  'verarbeitet und genutzt werden. Sie sind zudem darauf hingewiesen worden, dass die Erhebung,',
-  'Verarbeitung und Nutzung Ihrer Daten auf freiwilliger Basis erfolgt und die Einwilligung auch',
-  'verweigert werden kann. Die Verweigerung der Einwilligung führt dazu, dass keine Pressemitteilung',
-  'veröffentlicht wird. Es besteht jederzeit die Möglichkeit, die Einwilligung zu widerrufen. Mit der',
-  'Unterschrift der Soko-Mitarbeiterin/des Soko-Mitarbeiters wird bestätigt, dass die Einwilligung zur',
-  'Verarbeitung der personenbezogenen Daten mündlich/telefonisch gegeben wurde.'
-];
+export const SOKO_PRIVACY_TEXT = String.raw`
+  Das Soko-Mitglied hat die betroffene Person darauf hingewiesen, dass
+  die im Rahmen der vorstehend genannten Zwecke erhobenen personenbezogenen Daten unter
+  Beachtung der EU-Datenschutzgrundverordnung und des Berliner Datenschutzgesetzes erhoben,
+  verarbeitet und genutzt werden. Zudem wurde darauf hingewiesen, dass die Erhebung,
+  Verarbeitung und Nutzung ihrer Daten auf freiwilliger Basis erfolgt und die Einwilligung auch
+  verweigert werden kann. Die Verweigerung der Einwilligung führt dazu, dass keine Pressemitteilung
+  veröffentlicht wird. Es besteht jederzeit die Möglichkeit, die Einwilligung zu widerrufen. Mit der
+  Unterschrift des Soko-Mitglieds wird bestätigt, dass die Einwilligung zur Verarbeitung der Daten
+  gegeben wurde.
+`.replace(/\s+/g, ' ').trim();
 
-export const SOKO_PRIVACY_TEXT = lines.join('\n');
-export const SOKO_PRIVACY_TEXT_HTML = lines.map(escapeHtml).join('\n');
+export const SOKO_PRIVACY_TEXT_HTML = escapeHtml(SOKO_PRIVACY_TEXT);
